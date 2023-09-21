12586cde048bcc3eb003920764037830f06813fa
ReinierGeppaard <2103785geppaard@zuyd.nl>
2023-09-21 11:59:51 +0200

    test

---
+++ posenet/src/App.js
@@ -0,0 +1,92 @@
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
+import { drawKeypoints, drawSkeleton } from "./utilities";
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const runPosenet = async () => {
+      const net = await posenet.load({
+        inputResolution: { width: 640, height: 480 },
+        scale: 0.5,
+      });
+
+      const detect = async () => {
+        if (
+          typeof webcamRef.current !== "undefined" &&
+          webcamRef.current !== null &&
+          webcamRef.current.video.readyState === 4
+        ) {
+          const video = webcamRef.current.video;
+
+          // Create a TensorFlow.js tensor from the video feed
+          const input = tf.browser.fromPixels(video);
+          const pose = await net.estimateSinglePose(input);
+          input.dispose(); // Dispose of the input tensor when done
+
+          const ctx = canvasRef.current.getContext("2d");
+          const videoWidth = video.videoWidth;
+          const videoHeight = video.videoHeight;
+
+          webcamRef.current.video.width = videoWidth;
+          webcamRef.current.video.height = videoHeight;
+          canvasRef.current.width = videoWidth;
+          canvasRef.current.height = videoHeight;
+
+          drawKeypoints(pose.keypoints, 0.5, ctx, videoWidth / 640);
+          drawSkeleton(pose.keypoints, 0.5, ctx, videoWidth / 640);
+        }
+      };
+
+      // Call detect function periodically
+      const intervalId = setInterval(detect, 100);
+
+      // Clean up the interval when the component unmounts
+      return () => clearInterval(intervalId);
+    };
+
+    runPosenet();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
